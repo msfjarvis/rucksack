af6d93ef7f15f07e23f9071626afbf30fa219dc0
Harsh Shandilya <me@msfjarvis.dev>
2022-08-10 12:34:22 +0530

    feat: await all subscriptions together

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod watch;
 use crate::config::{get_path, RootConfig};
 use crate::watch::generate_subscriptions;
 use anyhow::Result;
+use futures::future::select_all;
 use tracing::metadata::LevelFilter;
 use tracing::{error, info, trace};
 use watchman_client::{prelude::*, SubscriptionData};
@@ -33,18 +34,21 @@ async fn run() -> Result<()> {
     }
 
     loop {
-        for sub in subs.iter_mut() {
-            let item = sub.next().await?;
-            match item {
-                SubscriptionData::FilesChanged(event) => {
-                    if let Some(files) = event.files {
-                        for file in files.iter() {
-                            info!(?file);
-                        }
+        let subscription_futures = subs
+            .iter_mut()
+            .map(|sub| Box::pin(sub.next()))
+            .collect::<Vec<_>>();
+        let (resolved, index, _) = select_all(subscription_futures).await;
+        let resolved = resolved?;
+        match resolved {
+            SubscriptionData::FilesChanged(event) => {
+                if let Some(files) = event.files {
+                    for file in files.iter() {
+                        info!(?file);
                     }
                 }
-                _ => trace!(?item),
-            };
-        }
+            }
+            _ => trace!(?resolved),
+        };
     }
 }
