e41e89ae3d9c474c8a830a11b4603a63c35d6dc3
Harsh Shandilya <me@msfjarvis.dev>
2022-12-17 05:02:56 +0000

    feat(nix): expand default checks and switch CI to Flakes
    Fixes SDP-24

--- flake.nix
+++ flake.nix
@@ -3,19 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-
     flake-utils.url = "github:numtide/flake-utils";
-
-    advisory-db = {
-      url = "github:rustsec/advisory-db";
-      flake = false;
-    };
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -23,6 +11,18 @@
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
       };
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
   };
 
@@ -35,13 +35,7 @@
           overlays = [ (import rust-overlay) ];
         };
 
-        rustStable = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-          targets =
-            pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-            [ "x86_64-unknown-linux-gnu" ];
-        };
+        rustStable = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
         src = craneLib.cleanCargoSource ./.;
         cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs; };
@@ -51,35 +45,25 @@
           inherit src;
           doCheck = false;
         };
-      in {
-        checks = { inherit file-collector; };
 
-        # Run clippy (and deny all warnings) on the crate source,
-        # again, resuing the dependency artifacts from above.
-        #
-        # Note that this is done as a separate derivation so that
-        # we can block the CI if there are issues here, but not
-        # prevent downstream consumers from building our crate by itself.
         file-collector-clippy = craneLib.cargoClippy {
           inherit cargoArtifacts src buildInputs;
           cargoClippyExtraArgs = "--all-targets -- --deny warnings";
         };
 
-        file-collector-doc = craneLib.cargoDoc { inherit cargoArtifacts src; };
-
-        # Check formatting
         file-collector-fmt = craneLib.cargoFmt { inherit src; };
 
-        # Audit dependencies
         file-collector-audit = craneLib.cargoAudit { inherit src advisory-db; };
 
-        # Run tests with cargo-nextest
-        # Consider setting `doCheck = false` on `file-collector` if you do not want
-        # the tests to run twice
         file-collector-nextest = craneLib.cargoNextest {
           inherit cargoArtifacts src buildInputs;
           partitions = 1;
           partitionType = "count";
+        };
+      in {
+        checks = {
+          inherit file-collector file-collector-audit file-collector-clippy
+            file-collector-fmt file-collector-nextest;
         };
 
         packages.default = file-collector;
@@ -89,7 +73,12 @@
         devShells.default = pkgs.mkShell {
           inputsFrom = builtins.attrValues self.checks;
 
-          nativeBuildInputs = with pkgs; [ cargo-deny cargo-release rustStable watchman ];
+          nativeBuildInputs = with pkgs; [
+            cargo-deny
+            cargo-release
+            rustStable
+            watchman
+          ];
         };
       });
 }
