ffbeae4a076efb41868ca0e87bb77983fb184166
Harsh Shandilya <me@msfjarvis.dev>
2026-05-29 22:37:31 +0530

    fix: bump to Rust 1.96.0

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
 
         rustStable = (import fenix { inherit pkgs; }).fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
+          sha256 = "sha256-mvUGEOHYJpn3ikC5hckneuGixaC+yGrkMM/liDIDgoU=";
         };
 
         craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
@@ -114,6 +114,7 @@
             fenix.packages.${system}.rust-analyzer
             gcc
             rustStable
+          ] ++ pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
             watchman
           ];
         };
